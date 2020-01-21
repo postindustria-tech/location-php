02512abbf902364ec2250f04f45835316793b965
Ben51Degrees <ben@51degrees.com>
2020-01-21 11:20:35 +0000

    DOC/EXMAPLE: Updated readme and added combining services example.

---
+++ FiftyOne.Pipeline.GeoLocation/examples/cloud/combiningServices.php
@@ -0,0 +1,101 @@
+<?php
+/* *********************************************************************
+ * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
+ * Copyright 2019 51 Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY.
+ *
+ * This Original Work is licensed under the European Union Public Licence (EUPL) 
+ * v.1.2 and is subject to its terms as set out below.
+ *
+ * If a copy of the EUPL was not distributed with this file, You can obtain
+ * one at https://opensource.org/licenses/EUPL-1.2.
+ *
+ * The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
+ * amended by the European Commission) shall be deemed incompatible for
+ * the purposes of the Work and the provisions of the compatibility
+ * clause in Article 5 of the EUPL shall not apply.
+ * 
+ * If using the Work as, or as part of, a network application, by 
+ * including the attribution notice(s) required under Article 5 of the EUPL
+ * in the end user terms of the application under an appropriate heading, 
+ * such notice(s) shall fulfill the requirements of that article.
+ * ********************************************************************* */
+
+/**
+ * @example cloud/combiningServices.php
+ *
+ * Example of using 51Degrees geolocation alongside device detection.
+ *
+ * The example shows how to:
+ *
+ * 1. Build a new Pipeline to use cloud geolocation engine, adding a cloud
+ * device detection engine.
+ * ```
+ * $builder = new geoLocationPipelineBuilder(array(
+ *     // Obtain a resource key from https://configure.51degrees.com
+ *     "resourceKey" => "",
+ *     "type" => "location",
+ *     "restrictedProperties" => array() // All properties by default
+ * ));
+ * $builder->add(new deviceDetectionCloud());
+ * $pipeline = $builder->build();
+ * ```
+ *
+ * 2. Create a new flow data instance ready to be populated with evidence for the
+ * Pipeline.
+ * ```
+ * $flowData = pipeline->createFlowData();
+ * ```
+ *
+ * 3. Process an HTTP request to retrieve the values associated
+ * with the request for the selected properties.
+ * ```
+ * $flowData->evidence->setFromWebRequest();
+ * $result = $flowData->process();
+ * ```
+ *
+ * 4. Extract the value of a property as a string from the results.
+ * ```
+ * echo "Country: " . $result->get("location")->get("country") . "<br/>";
+ * echo "IsMobile: " . $result->get("device")->get("ismobile") . "<br/>";
+ * ```
+ *
+ * 5. Add the JavaScript required to get extra information from the device. This is
+ * needed to get the longitude and latitude.
+ * ```
+ * echo "<script>";
+ * echo $result->get("javascriptbundler")->get("javascript");
+ * echo "</script>";
+ * ```
+ */
+
+require(__DIR__ . "/../../vendor/autoload.php");
+
+use fiftyone\pipeline\geolocation\geoLocation;
+use fiftyone\pipeline\devicedetection\deviceDetectionCloud;
+
+// Create a simple pipeline to access the engine with.
+$builder = new geoLocationPipelineBuilder(array(
+    // Obtain a resource key from https://configure.51degrees.com
+    "resourceKey" => "",
+    "type" => "location",
+    "restrictedProperties" => array() // All properties by default
+));
+
+// Add device detection.
+$builder->add(new deviceDetectionCloud());
+
+$pipeline = $builder->build();
+
+$flowData = $pipeline->createFlowData();
+
+$flowData->evidence->setFromWebRequest();
+
+$result = $flowData->process();
+
+echo "<script>";
+echo $result->get("javascriptbundler")->get("javascript");
+echo "</script>";
+
+echo "Country: " . $result->get("location")->get("country") . "<br/>";
+echo "IsMobile: " . $result->get("device")->get("ismobile") . "<br/>";
--- readme.md
+++ readme.md
@@ -1,23 +1,52 @@
-![51Degrees](https://51degrees.com/DesktopModules/FiftyOne/Distributor/Logo.ashx?utm_source=github&utm_medium=repository&utm_content=readme_main&utm_campaign=php-open-source "Data rewards the curious") **Location Service for PHP Pipeline**
+# 51Degrees Geo-Location Engines
+
+![51Degrees](https://51degrees.com/DesktopModules/FiftyOne/Distributor/Logo.ashx?utm_source=github&utm_medium=repository&utm_content=readme_main&utm_campaign=php-open-source "Data rewards the curious") **Pipeline API**
 
 [Developer Documentation](https://docs.51degrees.com?utm_source=github&utm_medium=repository&utm_content=documentation&utm_campaign=php-open-source "developer documentation")
 
 ## Introduction
 
-A Geo location solution, backed by several different vendors, is available via the 51Degrees cloud service. 
-Regardless of vendor, this currently requires longitude and latitude information from the client device, which is retrieved seamlessly by the pipeline.
+This repository contains the geo-location engines for the PHP implementation of the Pipeline API.
 
-#### Building
+## Pre-requesites
 
-To build a pipeline containing everything required to start using geolocation, simply [obtain a resource key](https://configure.51degrees.com) and use it with the ``geolocationPipelineBuilder`` to construct a pipeline.
+The Pipeline engines are written in PHP and target 5 and 7.
+
+## Packages
+- **geoLocation** - A Node.js engine which retrieves geo-location results by consuming data from the 51Degrees cloud service.
+- **geoLocationPipelineBuilder** - Contains the geo-location engine builders.
+
+## Installation
+
+You can either reference the projects in this repository or you can reference the [Composer][composer] packages in your project:
+
+```
+"require": {
+    "51degrees/fiftyone.geolocation": "@dev",
+    ...
+```
+
+Make sure to select the latest version from [Composer.][composer]
 
 #### Configuration Options
 
  - String ``type`` - The name of the type of geolocation service to use.
  - String ``resourceKey`` - Resource Key is evidence used within the Cloud service for monitoring usage. [Obtain a resource key](https://configure.51degrees.com).
- - String ``licenceKeys`` - The license key supplied with your available product.
  - Array ``restrictedProperties`` - The properties to populate values for in the result (all are populated by default).
 
-### Examples
+## Examples
 
-Usage examples are available in ``FiftyOne.Pipeline.GeoLocation``
+Examples can be found in the `FiftyOne.Pipeline.GeoLocation/` folder. See below for a list of examples.
+
+|Example|Description|Implemtation|
+|-------|-----------|------------|
+|gettingStarted.php|This example uses geo-location to determine the country from a longitude and latidude.|Cloud|
+|configureFromFile.php|This examples creates a pipeline from a configuration file to set up geo-location.|Cloud|
+|combiningServices.php|This example uses geo-location alongside device detection to determine the country and device.|Cloud|
+
+## Project documentation
+
+For complete documentation on the Pipeline API and associated engines, see the [51Degrees documentation site][Documentation].
+
+[Documentation]: https://docs.51degrees.com
+[composer]: https://packagist.org/packages/51degrees/fiftyone.geolocation
